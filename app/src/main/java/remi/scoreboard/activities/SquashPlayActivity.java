6a94112ff0ef0bb362559c9696e23b754c6f352d
rlatapy <remi.latapy@gmail.com>
2017-06-02 00:45:31 +0200

    Add dialog_match_score

--- app/src/main/java/remi/scoreboard/activities/SquashPlayActivity.java
+++ app/src/main/java/remi/scoreboard/activities/SquashPlayActivity.java
@@ -1,8 +1,14 @@
 package remi.scoreboard.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.util.Pair;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,7 +24,8 @@ import remi.scoreboard.model.Player;
 public class SquashPlayActivity extends GameActivity {
 
     // Typedef
-    private class MatchDay extends ArrayList<Pair<Player, Player>> {}
+    private class Match extends Pair<Player, Player> { Match(Player pOne, Player pTwo) { super(pOne, pTwo); }}
+    private class MatchDay extends ArrayList<Match> {}
     private class Championship extends ArrayList<MatchDay> {}
 
     private TextView textviewMatchdayNum;
@@ -63,14 +70,14 @@ public class SquashPlayActivity extends GameActivity {
 
             int playerIdx = day % playersSize;
 
-            matchDay.add(new Pair<>(players.get(playerIdx), playerList.get(0)));
+            matchDay.add(new Match(players.get(playerIdx), playerList.get(0)));
 
             for (int idx = 1; idx < playerList.size() / 2; idx++)
             {
                 int firstPlayer = (day + idx) % playersSize;
                 int secondPlayer = (day  + playersSize - idx) % playersSize;
 
-                matchDay.add(new Pair<>(players.get(firstPlayer), players.get(secondPlayer)));
+                matchDay.add(new Match(players.get(firstPlayer), players.get(secondPlayer)));
             }
 
             championship.add(matchDay);
@@ -84,10 +91,17 @@ public class SquashPlayActivity extends GameActivity {
                 view = getLayoutInflater().inflate(R.layout.item_card_matchday, cardContainerView, false);
                 findViews(view);
                 fillTextViews(i, j);
+                buildAlertDialog(view, championship.get(i).get(j),i+1);
                 cardContainerView.addView(view);
             }
         }
     }
+
+//    private void updateViews(PhaseDixPlayer player) {
+//        findViews(cardContainerView.getChildAt(player.getNum() - 1));
+//        playerPhase.setText("Phase " + player.getPhase());
+//        playerPoints.setText(player.getPoints() + " points");
+//    }
 
     private void fillTextViews(int matchdayNum, int matchNum) {
         textviewMatchdayNum.setText("Matchday " + (matchdayNum + 1));
@@ -99,5 +113,51 @@ public class SquashPlayActivity extends GameActivity {
         textviewMatchdayNum = (TextView) view.findViewById(R.id.matchday_num);
         textviewPlayerOneName = (TextView) view.findViewById(R.id.player1_name);
         textviewPlayerTwoName = (TextView) view.findViewById(R.id.player2_name);
+    }
+
+    private void buildAlertDialog(View view, final Match currentMatch, final int matchdayNumber) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View dialogMatchScore = getLayoutInflater().inflate(R.layout.dialog_match_score, (ViewGroup) v.getRootView(), false);
+                ((TextView)dialogMatchScore.findViewById(R.id.player1_name)).setText(currentMatch.first.getName());
+                ((TextView)dialogMatchScore.findViewById(R.id.player2_name)).setText(currentMatch.second.getName());
+                (new AlertDialog.Builder(SquashPlayActivity.this))
+                        .setCancelable(true)
+                        .setTitle("Matchday " + matchdayNumber)
+                        .setView(dialogMatchScore)
+                        .setPositiveButton("Valider", getValidateListener(currentMatch))
+                        .setNegativeButton("Annuler", getCancelListener())
+                        .show();
+            }
+        });
+    }
+
+    @NonNull
+    private DialogInterface.OnClickListener getCancelListener() {
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        };
+    }
+
+    @NonNull
+    private DialogInterface.OnClickListener getValidateListener(final Match currentMatch) {
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String scorePlayerOne = ((EditText) ((AlertDialog) dialog).findViewById(R.id.player1_score)).getText().toString();
+                String scorePlayerTwo = ((EditText) ((AlertDialog) dialog).findViewById(R.id.player2_score)).getText().toString();
+                if (!scorePlayerOne.isEmpty() && !scorePlayerTwo.isEmpty()) {
+                    Log.d("SCORE", String.valueOf(Integer.parseInt(scorePlayerOne)));
+                    Log.d("SCORE", String.valueOf(Integer.parseInt(scorePlayerTwo)));
+               //     ((PhaseDixPlayer) currentPlayer).addPoints(Integer.parseInt(pointsText));
+                }
+
+           //     updateViews(((PhaseDixPlayer) currentPlayer));
+            }
+        };
     }
 }
