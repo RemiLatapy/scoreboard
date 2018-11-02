44c7008c827e8c0fd342f32ad705a3021b001acc
rlatapy <remi.latapy@gmail.com>
2018-11-02 15:20:41 +0100

    Add player entity
    Add IDs

--- app/src/main/java/remi/scoreboard/data/User.kt
+++ app/src/main/java/remi/scoreboard/data/User.kt
@@ -1,13 +1,11 @@
 package remi.scoreboard.data
 
 import io.realm.RealmObject
-import io.realm.annotations.Ignore
-import io.realm.annotations.RealmClass
-import io.realm.annotations.RealmNamingPolicy
-import io.realm.annotations.Required
+import io.realm.annotations.*
 
 @RealmClass(name = "users", fieldNamingPolicy = RealmNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 open class User(
+    @PrimaryKey var id: Long,
     @Required var firstName: String = "default_first_name",
     var lastName: String = "default_last_name",
     var email: String = "",
