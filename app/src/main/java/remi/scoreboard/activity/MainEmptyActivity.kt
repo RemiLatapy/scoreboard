a68c132fe88bcbc222d77d7b41b60681602070e5
rlatapy <remi.latapy@gmail.com>
2018-11-04 17:47:04 +0100

    Add new main empty activity

---
+++ app/src/main/java/remi/scoreboard/activity/MainEmptyActivity.kt
@@ -0,0 +1,23 @@
+package remi.scoreboard.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainEmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val token = "fake token"
+
+        val activityIntent = if (token.isNotEmpty()) {
+            Intent(this, MainActivity::class.java)
+        } else {
+            Intent(this, LoginActivity::class.java)
+        }
+
+        startActivity(activityIntent)
+        finish()
+    }
+}
