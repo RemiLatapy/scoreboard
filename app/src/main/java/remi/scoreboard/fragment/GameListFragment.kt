068be72263d4b05d5f031f5a7bf875e71c3ce6e3
rlatapy <remi.latapy@gmail.com>
2018-11-15 10:41:12 +0100

    Migrate game adapter to fastadapter

--- app/src/main/java/remi/scoreboard/fragment/GameListFragment.kt
+++ app/src/main/java/remi/scoreboard/fragment/GameListFragment.kt
@@ -8,12 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
-import remi.scoreboard.adapter.GameAdapter
-import remi.scoreboard.data.Game
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
 import remi.scoreboard.databinding.FragmentGameListBinding
+import remi.scoreboard.fastadapter.item.GameItem
 import remi.scoreboard.viewmodel.GameViewModel
 
-class GameListFragment : Fragment(), GameAdapter.GameSelectedCallback {
+class GameListFragment : Fragment() {
 
     private lateinit var gameViewModel: GameViewModel
 
@@ -22,17 +23,21 @@ class GameListFragment : Fragment(), GameAdapter.GameSelectedCallback {
 
         gameViewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
-        val gameListAdapter = GameAdapter(this)
-        binding.recycler.adapter = gameListAdapter
+
+        val gameItemAdapter = ItemAdapter<GameItem>()
+        val fastAdapter: FastAdapter<GameItem> = FastAdapter.with(gameItemAdapter)
+        fastAdapter.withOnClickListener { _, _, gameItem, _ ->
+            val action = GameListFragmentDirections.actionChoosePlayers(gameItem.game.id)
+            findNavController().navigate(action)
+            true
+        }
+
+        binding.recycler.adapter = fastAdapter
+
         gameViewModel.allGames.observe(this,
-            Observer { gameList -> gameList?.let { gameListAdapter.submitList(it) } })
+            Observer { gameList -> gameList?.let { gameItemAdapter.set(it.map { GameItem(it) }) } })
 
         return binding.root
-    }
-
-    override fun onGameSelected(game: Game) {
-        val action = GameListFragmentDirections.actionChoosePlayers(game.id)
-        findNavController().navigate(action)
     }
 }
 
